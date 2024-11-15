42c920091fa31b23eb0ad8e6a5a4b6bf20438b53
DonMarat <marat_marv_2005@mail.ru>
2024-11-16 00:59:45 +0300

    Сделал модульные тесты к второй лабораторной

--- TestsforProject/Test1.cs
+++ TestsforProject/Test1.cs
@@ -1,72 +1,110 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MatrixLabor2;
 using Matrix;
 using System;
 
-namespace MatrixTests
+namespace MatrixApp.Tests
 {
     [TestClass]
     public class MatrixTests
     {
         [TestMethod]
-        public void MatrixInitializationTest()
+        public void CorrectRowNumberInMatrixTest()
         {
-            var matrix = new Matrix<double>(3, 3);
-            Assert.AreEqual(3, matrix.RowCount);
-            Assert.AreEqual(3, matrix.ColumnCount);
+            var matrixA = new Matrix<int>(2, 3);
+            Assert.AreEqual(2, matrixA.RowCount);
         }
 
         [TestMethod]
-        public void MatrixIndexerTest()
+        public void CorrectColumnNumberInMatrixTest()
         {
-            var matrix = new Matrix<int>(2, 2);
-            matrix[0, 0] = 5;
-            matrix[0, 1] = 10;
-            matrix[1, 0] = 15;
-            matrix[1, 1] = 20;
-
-            Assert.AreEqual(5, matrix[0, 0]);
-            Assert.AreEqual(10, matrix[0, 1]);
-            Assert.AreEqual(15, matrix[1, 0]);
-            Assert.AreEqual(20, matrix[1, 1]);
+            var matrixA = new Matrix<int>(3, 3);
+            Assert.AreEqual(3, matrixA.ColumnCount);
         }
 
         [TestMethod]
-        public void MatrixAdditionTest()
+        public void MatrixAddition_ValidInput_ReturnsCorrectResult()
         {
             var matrixA = new Matrix<int>(2, 2);
+            var matrixB = new Matrix<int>(2, 2);
+
             matrixA[0, 0] = 1;
             matrixA[0, 1] = 2;
             matrixA[1, 0] = 3;
             matrixA[1, 1] = 4;
 
-            var matrixB = new Matrix<int>(2, 2);
             matrixB[0, 0] = 5;
             matrixB[0, 1] = 6;
             matrixB[1, 0] = 7;
             matrixB[1, 1] = 8;
 
-            Matrix<int> result = matrixA + matrixB;
+            var expected = new Matrix<int>(2, 2);
+            expected[0, 0] = 6;
+            expected[0, 1] = 8;
+            expected[1, 0] = 10;
+            expected[1, 1] = 12;
 
-            Assert.AreEqual(6, result[0, 0]);
-            Assert.AreEqual(8, result[0, 1]);
-            Assert.AreEqual(10, result[1, 0]);
-            Assert.AreEqual(12, result[1, 1]);
+            var result = matrixA + matrixB;
+
+            for (int i = 0; i < expected.RowCount; i++)
+            {
+                for (int j = 0; j < expected.ColumnCount; j++)
+                {
+                    Assert.AreEqual(expected[i, j], result[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MatrixMultiplication_ValidInput_ReturnsCorrectResult()
+        {
+            var matrixA = new Matrix<int>(2, 2);
+            var matrixB = new Matrix<int>(2, 2);
+
+            matrixA[0, 0] = 1;
+            matrixA[0, 1] = 2;
+            matrixA[1, 0] = 3;
+            matrixA[1, 1] = 4;
+
+            matrixB[0, 0] = 5;
+            matrixB[0, 1] = 6;
+            matrixB[1, 0] = 7;
+            matrixB[1, 1] = 8;
+
+            var expected = new Matrix<int>(2, 2);
+            expected[0, 0] = 19;
+            expected[0, 1] = 22;
+            expected[1, 0] = 43;
+            expected[1, 1] = 50;
+
+            var result = matrixA * matrixB;
+
+            for (int i = 0; i < expected.RowCount; i++)
+            {
+                for (int j = 0; j < expected.ColumnCount; j++)
+                {
+                    Assert.AreEqual(expected[i, j], result[i, j]);
+                }
+            }
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void MatrixAdditionSizeMismatchTest()
+        public void MatrixAddition_DifferentDimensions_ThrowsException()
         {
-            var matrixA = new Matrix<int>(2, 3);
-            var matrixB = new Matrix<int>(3, 3);
-            var result = matrixA + matrixB; // Должно выбросить исключение
+            var matrixA = new Matrix<int>(1, 2);
+            var matrixB = new Matrix<int>(2, 2);
+
+            var result = matrixA + matrixB;
         }
 
         [TestMethod]
-        public void MatrixMultiplicationTest()
+        [ExpectedException(typeof(ArgumentException))]
+        public void MatrixMultiplication_IncompatibleDimensions_ThrowsException()
         {
+            // Создаем первую матрицу размером 2x3
             var matrixA = new Matrix<int>(2, 3);
+
+            // Заполняем первую матрицу
             matrixA[0, 0] = 1;
             matrixA[0, 1] = 2;
             matrixA[0, 2] = 3;
@@ -74,78 +112,137 @@ namespace MatrixTests
             matrixA[1, 1] = 5;
             matrixA[1, 2] = 6;
 
-            var matrixB = new Matrix<int>(3, 2);
+            // Создаем вторую матрицу размером 2x2
+            var matrixB = new Matrix<int>(2, 2);
+
+            // Заполняем вторую матрицу
             matrixB[0, 0] = 7;
             matrixB[0, 1] = 8;
             matrixB[1, 0] = 9;
             matrixB[1, 1] = 10;
-            matrixB[2, 0] = 11;
-            matrixB[2, 1] = 12;
 
-            Matrix<int> result = matrixA * matrixB;
+            // Пытаемся выполнить умножение матриц с несовместимыми размерами
+            var result = matrixA * matrixB;  
+        }
 
-            Assert.AreEqual(58, result[0, 0]);
-            Assert.AreEqual(64, result[0, 1]);
-            Assert.AreEqual(139, result[1, 0]);
-            Assert.AreEqual(154, result[1, 1]);
+
+        [TestMethod]
+        public void MatrixIndexing_GetAndSetValues_ReturnsCorrectValues()
+        {
+            var matrix = new Matrix<int>(2, 2);
+            matrix[0, 0] = 42;
+
+            Assert.AreEqual(42, matrix[0, 0]);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void MatrixMultiplicationSizeMismatchTest()
+        public void MatrixEquality_SameMatrices_ReturnsTrue()
         {
             var matrixA = new Matrix<int>(2, 2);
-            var matrixB = new Matrix<int>(3, 2); // Ошибка: не совместимы для умножения
-            var result = matrixA * matrixB; // Должно выбросить исключение
+            var matrixB = new Matrix<int>(2, 2);
+
+            matrixA[0, 0] = 1;
+            matrixA[0, 1] = 2;
+            matrixA[1, 0] = 3;
+            matrixA[1, 1] = 4;
+
+            matrixB[0, 0] = 1;
+            matrixB[0, 1] = 2;
+            matrixB[1, 0] = 3;
+            matrixB[1, 1] = 4;
+
+            for (int i = 0; i < matrixA.RowCount; i++)
+            {
+                for (int j = 0; j < matrixA.ColumnCount; j++)
+                {
+                    Assert.AreEqual(matrixA[i, j], matrixB[i, j]);
+                }
+            }
+        }
+        [TestMethod]
+        public void MatrixAdd_WithFloatValues_ReturnsCorrectResult()
+        {
+            // Создание матриц размером 2x2
+            var matrixA = new Matrix<float>(2, 2);
+            var matrixB = new Matrix<float>(2, 2);
+
+            // Заполнение значениями для первой матрицы
+            matrixA[0, 0] = 4.5f;
+            matrixA[0, 1] = 6.7f;
+            matrixA[1, 0] = 2.3f;
+            matrixA[1, 1] = 8.9f;
+
+            // Заполнение значениями для второй матрицы
+            matrixB[0, 0] = 1.1f;
+            matrixB[0, 1] = 2.2f;
+            matrixB[1, 0] = 3.3f;
+            matrixB[1, 1] = 4.4f;
+
+            // Ожидаемый результат после сложения
+            var expected = new Matrix<float>(2, 2);
+            expected[0, 0] = 5.6f;
+            expected[0, 1] = 8.9f;
+            expected[1, 0] = 5.6f;
+            expected[1, 1] = 13.3f;
+
+            // Выполнение операции сложения
+            var result = matrixA + matrixB;
+
+            // Проверка правильности сложения матриц
+            for (int i = 0; i < expected.RowCount; i++)
+            {
+                for (int j = 0; j < expected.ColumnCount; j++)
+                {
+                    Assert.AreEqual(expected[i, j], result[i, j], 0.0001f);
+                }
+            }
         }
 
         [TestMethod]
-        public void EmptyMatrixTest()
+        public void MatrixEquality_DifferentMatrices_ReturnsFalse()
         {
-            var matrix = new Matrix<int>(0, 0);
-            Assert.AreEqual(0, matrix.RowCount);
-            Assert.AreEqual(0, matrix.ColumnCount);
+            var matrixA = new Matrix<int>(2, 2);
+            var matrixB = new Matrix<int>(2, 2);
+
+            matrixA[0, 0] = 1;
+            matrixA[0, 1] = 2;
+            matrixA[1, 0] = 3;
+            matrixA[1, 1] = 4;
+
+            matrixB[0, 0] = 5;
+            matrixB[0, 1] = 6;
+            matrixB[1, 0] = 7;
+            matrixB[1, 1] = 8;
+
+            for (int i = 0; i < matrixA.RowCount; i++)
+            {
+                for (int j = 0; j < matrixA.ColumnCount; j++)
+                {
+                    Assert.AreNotEqual(matrixA[i, j], matrixB[i, j]);
+                }
+            }
         }
-
-        [TestMethod]
-        public void MatrixPopulateTest()
-        {
-            var matrix = new Matrix<int>(2, 2);
-            matrix.Populate((i, j) => i * j); // Заполняем матрицу значениями: (0,0), (0,1), (1,0), (1,1)
-
-            Assert.AreEqual(0, matrix[0, 0]);
-            Assert.AreEqual(0, matrix[0, 1]);
-            Assert.AreEqual(0, matrix[1, 0]);
-            Assert.AreEqual(1, matrix[1, 1]);
-        }
-
-        [TestMethod]
-        public void MatrixExportToCsvTest()
-        {
-            var matrix = new Matrix<double>(2, 2);
-            matrix[0, 0] = 1.1;
-            matrix[0, 1] = 2.2;
-            matrix[1, 0] = 3.3;
-            matrix[1, 1] = 4.4;
-
-            string filePath = "test.csv";
-            matrix.ExportToCsv(filePath);
-
-            Assert.IsTrue(System.IO.File.Exists(filePath));
-
-            var lines = System.IO.File.ReadAllLines(filePath);
-            Assert.AreEqual("1,10;2,20", lines[0]);
-            Assert.AreEqual("3,30;4,40", lines[1]);
-
-            System.IO.File.Delete(filePath); // Удаление файла после теста
-        }
-
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void MatrixPopulateWithInvalidFunctionTest()
+        public void ArgumentExceptionInMultiplicationWhenOneMatrixIsEmptyTest()
         {
-            var matrix = new Matrix<int>(2, 2);
-            matrix.Populate((i, j) => throw new InvalidOperationException()); // Невозможность заполнить из-за ошибки в функции
+            // Создаем матрицу с нулевыми размерами (0x0)
+            var matrixA = new Matrix<int>(0, 0);  // Передаем 0 строк и 0 столбцов
+
+            // Создаем матрицу размером 2x2
+            var matrixB = new Matrix<int>(2, 2);
+            matrixB[0, 0] = 7;
+            matrixB[0, 1] = 2;
+            matrixB[1, 0] = 1;
+            matrixB[1, 1] = 0;
+
+            // Пытаемся умножить пустую матрицу на обычную, что должно вызвать исключение
+            var result = matrixA * matrixB;
         }
+
+
+
+
     }
 }
+
